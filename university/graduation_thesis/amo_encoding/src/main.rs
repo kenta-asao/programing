51d0e742c843e43bb48dfe868564138903e8edb7
kenta-asao <kenta.a.3.23@gmail.com>
2024-10-09 20:35:57 +0900

    20241009

--- university/graduation_thesis/amo_encoding/src/main.rs
+++ university/graduation_thesis/amo_encoding/src/main.rs
@@ -6,29 +6,71 @@ use std::io::Write;
 use std::process::{Command, Stdio};
 
 fn main() {
-    pairwise_encoding(5)
+    let n = 5;
+    let encoding = 2;
+    if encoding == 1 {
+        pairwise_encoding(n);
+    }
+    else {
+        ladder_ecncoding(n);
+    }
 }
 
-fn pairwise_encoding(n: i32) {//pairwise encoding -> encoding.cnfにDIMACS形式で出力
+//pairwise encoding -> encoding.cnfにDIMACS形式で出力
+fn pairwise_encoding(n: i32) {
     let path = "encoding.cnf";
     let mut file = File::create(path).expect("file not found.");
 
     let variables = n;
-    let clauses = n*(n-1)/2;
+    let clauses = n * (n - 1) / 2;
 
     writeln!(file, "p cnf {} {}", variables, clauses).expect("cannot write.");
-    for i in 1..n+1 {
-        for j in i+1..n+1 {
-            writeln!{file, "-{} -{} 0", i,j}.expect("cannot write.");
+    for i in 1..n + 1 {
+        for j in i + 1..n + 1 {
+            writeln! {file, "-{} -{} 0", i,j}.expect("cannot write.");
         }
     }
 
     clasp();
 }
 
+fn ladder_ecncoding(n: i32) {
+    let path = "encoding.cnf";
+    let mut file = File::create(path).expect("file not found.");
+    let ladder_variable = n-1;
 
+    let variables = n + ladder_variable;
+    let clauses = n-2 + 3*n;
 
-fn clasp() {//claspの実行 -> result.txtに出力
+    writeln!(file, "p cnf {} {}", variables, clauses).expect("cannot write.");
+
+    //ladder valid clauses
+    for i in 1..ladder_variable {
+        writeln! {file, "-{} {} 0", n+i+1, n+i}.expect("cannot write.");
+    }
+
+    //channelling clauses
+    for i in 1..n+1 {
+        if i == 1 {
+            writeln! {file, "{} {} 0", n+i, i}.expect("cannot write.");
+            writeln! {file, "-{} -{} 0", i, n+i}.expect("cannot write.");
+        }
+        else if i == n {
+            writeln! {file, "-{} {} 0", n+i-1, i}.expect("cannot write.");
+            writeln! {file, "-{} {} 0", i, n+i-1}.expect("cannot write.");
+        }
+        else {
+            writeln! {file, "-{} {} {} 0", n+i-1, n+i, i}.expect("cannot write.");
+            writeln! {file, "-{} {} 0", i, n+i-1}.expect("cannot write.");
+            writeln! {file, "-{} -{} 0", i, n+i}.expect("cannot write.");
+        }        
+    }
+
+    clasp();
+}
+
+//claspの実行 -> result.txtに出力
+fn clasp() {
     let output_file = File::create("result.txt").expect("Failed to create file");
 
     let output = Command::new("clasp")
@@ -38,4 +80,3 @@ fn clasp() {//claspの実行 -> result.txtに出力
         .expect("failed");
     println!("{}", String::from_utf8_lossy(&output.stdout));
 }
-
