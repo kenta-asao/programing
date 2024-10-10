b1afa155671000e87f3a9e801c8b8811b93948b8
kenta-asao <kenta.a.3.23@gmail.com>
2024-10-10 15:47:12 +0900

    20241010

--- personal/rust/hello_cargo/target/debug/deps/hello_cargo-ce6c05f1317b7a42.d
+++
@@ -1,5 +0,0 @@
-/home/kenta/git/programing/rust/hello_cargo/target/debug/deps/hello_cargo-ce6c05f1317b7a42: src/main.rs
-
-/home/kenta/git/programing/rust/hello_cargo/target/debug/deps/hello_cargo-ce6c05f1317b7a42.d: src/main.rs
-
-src/main.rs:
--- personal/rust/hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/kenta/git/programing/rust/hello_cargo/target/debug/hello_cargo: /home/kenta/git/programing/rust/hello_cargo/src/main.rs
