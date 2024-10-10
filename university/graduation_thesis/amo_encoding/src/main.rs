fd391312341931f3ca583bbd3ddabd77f7135679
kenta-asao <kenta.a.3.23@gmail.com>
2024-10-10 15:11:07 +0900

    20241010

--- university/graduation_thesis/amo_encoding/src/main.rs
+++ university/graduation_thesis/amo_encoding/src/main.rs
@@ -6,13 +6,19 @@ use std::io::Write;
 use std::process::{Command, Stdio};
 
 fn main() {
-    let n = 5;
-    let encoding = 2;
+    let n = 3;
+    let encoding = 3;
     if encoding == 1 {
         pairwise_encoding(n);
     }
-    else {
+    else if encoding == 2 {
         ladder_ecncoding(n);
+    }
+    else if encoding == 3 {
+        binary_enncoding(n);
+    }
+    else {
+        relaxed_ladder_ecncoding(n);
     }
 }
 
@@ -63,10 +69,106 @@ fn ladder_ecncoding(n: i32) {
             writeln! {file, "-{} {} {} 0", n+i-1, n+i, i}.expect("cannot write.");
             writeln! {file, "-{} {} 0", i, n+i-1}.expect("cannot write.");
             writeln! {file, "-{} -{} 0", i, n+i}.expect("cannot write.");
+        }
+    }
+
+    clasp();
+}
+
+fn binary_enncoding(n: i32) {
+    let path = "encoding.cnf";
+    let mut file = File::create(path).expect("file not found.");
+
+    let variables = n + log2(n);
+    let clauses = n*log2(n);
+
+    writeln!(file, "p cnf {} {}", variables, clauses).expect("cannot write.");
+
+    for i in 1..log2(n)+1 {
+        for j in 1..n+1 {
+            if j == (i+1) {
+                writeln! {file, "-{} {} 0", j, n+i}.expect("cannot write.");
+            }
+            else {
+                writeln! {file, "-{} -{} 0", j, n+i}.expect("cannot write.");
+            }
+        }
+    }
+    clasp();
+}
+
+fn relaxed_ladder_ecncoding(n: i32) {
+    let path = "encoding.cnf";
+    let mut file = File::create(path).expect("file not found.");
+    let ladder_variable = n-1;
+
+    let variables = n + ladder_variable;
+    let clauses = n-2 + 3*n;
+
+    writeln!(file, "p cnf {} {}", variables, clauses).expect("cannot write.");
+
+    //ladder valid clauses
+    for i in 1..ladder_variable {
+        writeln! {file, "-{} {} 0", n+i+1, n+i}.expect("cannot write.");
+    }
+
+    //channelling clauses
+    for i in 1..n+1 {
+        if i == 1 {
+            writeln! {file, "-{} -{} 0", i, n+i}.expect("cannot write.");
+        }
+        else if i == n {
+            writeln! {file, "-{} {} 0", i, n+i-1}.expect("cannot write.");
+        }
+        else {
+            writeln! {file, "-{} {} 0", i, n+i-1}.expect("cannot write.");
+            writeln! {file, "-{} -{} 0", i, n+i}.expect("cannot write.");
         }        
     }
 
     clasp();
+}
+
+fn commander_encoding(n :i32) {
+    let path = "encoding.cnf";
+    let mut file = File::create(path).expect("file not found.");
+
+    let variables = n;
+    let clauses = n * (n - 1) / 2;
+
+    writeln!(file, "p cnf {} {}", variables, clauses).expect("cannot write.");
+}
+
+fn product_encoding(n :i32) {
+    let path = "encoding.cnf";
+    let mut file = File::create(path).expect("file not found.");
+
+    let variables = n;
+    let clauses = n * (n - 1) / 2;
+
+    writeln!(file, "p cnf {} {}", variables, clauses).expect("cannot write.");
+}
+
+fn bimander_encoding(n: i32) {
+    let path = "encoding.cnf";
+    let mut file = File::create(path).expect("file not found.");
+
+    let variables = n;
+    let clauses = n * (n - 1) / 2;
+
+    writeln!(file, "p cnf {} {}", variables, clauses).expect("cannot write.");
+}
+
+
+fn log2(num: i32) -> i32 {
+    let mut n: i32 = 0;
+    loop {
+        if n * 2 >= num {
+            break;
+        }
+        n += 1;
+    }
+    return n;
 }
 
 //claspの実行 -> result.txtに出力
@@ -80,3 +182,5 @@ fn clasp() {
         .expect("failed");
     println!("{}", String::from_utf8_lossy(&output.stdout));
 }
+
+
