8ef5bc7c64fd25bb5d68283616137b31d880198a
kenta-asao <kenta.a.3.23@gmail.com>
2024-10-17 08:51:34 +0900

    20241017

--- university/graduation_thesis/practise/target/debug/practise.d
+++ university/graduation_thesis/practise/target/debug/practise.d
@@ -1,1 +1,1 @@
-C:\github\programing\university\graduation_thesis\practise\target\debug\practise.exe: C:\github\programing\university\graduation_thesis\practise\src\main.rs
+/home/kenta/git/programing/university/graduation_thesis/practise/target/debug/practise: /home/kenta/git/programing/university/graduation_thesis/practise/src/main.rs
